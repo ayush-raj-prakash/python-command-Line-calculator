e5cbc86add16e5ac0ae57b426f15f0afc41f578e
Ayush Raj Prakash <ayushrajprakash2005@gmail.com>
2026-06-11 19:23:02 +0530

    Refactor calculator loop with error handling

--- main.py
+++ main.py
@@ -1,29 +1,33 @@
-while 1:
-    num1=float(input("Enter the first number:"))
-    num2=float(input("Enter the second number:"))
+while True:
+    try:
+        num1 = float(input("Enter the first number (or 'q' to quit): "))
+        num2 = float(input("Enter the second number: "))
+    except ValueError:
+        print("Invalid input! Please enter a valid number.")
+        continue
 
-    op=input("Enter the operator (+,-,*,/,// or %) :")
+    op = input("Enter the operator (+, -, *, /, //, %, **): ")
 
-    if  (op=='/' or op=='//' or op=='%') and num2==0:
-        print("Error! Denominator can not be zero.")
-
-    elif op=='+':
-        print(num1+num2)
-
-    elif op=='-':
-        print(num1-num2)
-
-    elif op=='*':
-        print(num1*num2)
-
-    elif op=='/':
-        print(num1/num2)
-
-    elif op=='//':
-        print(num1//num2)
-
-    elif op=='%':
-        print(num1%num2)
-
+    if (op in ('/', '//', '%')) and num2 == 0:
+        print("Error! Denominator cannot be zero.")
+    elif op == '+':
+        print(num1 + num2)
+    elif op == '-':
+        print(num1 - num2)
+    elif op == '*':
+        print(num1 * num2)
+    elif op == '/':
+        print(num1 / num2)
+    elif op == '//':
+        print(num1 // num2)
+    elif op == '%':
+        print(num1 % num2)
+    elif op == '**':
+        print(num1 ** num2)
     else:
-        print("Invalid Operator!!")
+        print("Invalid operator!")
+
+    again = input("Calculate again? (y/n): ")
+    if again.lower() != 'y':
+        print("Goodbye!")
+        break
